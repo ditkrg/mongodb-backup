bd12d246a89f5a0acdee57668621ab43d1f9b5b4
MohamadTahir <mohamad.tahir27@gmail.com>
2024-08-11 08:54:35 +0300

    rename param

--- internal/cli/helpers/functions.go
+++ internal/cli/helpers/functions.go
@@ -10,14 +10,14 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func ChooseDatabaseToRestore(s3Service *services.S3Service, ctx context.Context, bucket string, prefix string, validator func(string) bool) (backupToRestore string) {
+func ChooseDatabaseToRestore(s3Service *services.S3Service, ctx context.Context, bucket string, prefix string, showOption func(string) bool) (backupToRestore string) {
 	response := s3Service.List(ctx, bucket, prefix)
 
 	list := make([]huh.Option[string], 0)
 
 	for _, object := range response.Contents {
 		key := *object.Key
-		if validator(key) {
+		if showOption(key) {
 			list = append(list, huh.NewOption(key, key))
 		}
 	}
